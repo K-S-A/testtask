afae42ee323f1b6a60aa9f1eaa13018806a7b685
K-S-A <atillas@ukr.net>
2015-03-30 04:04:09 +0300

    Few link modifications - project_list_path to root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+root "project_lists#index"
+
   resources :project_lists do
     resources :todo_items do
       member do
@@ -8,5 +10,4 @@ Rails.application.routes.draw do
     end
   end
 
-  root "project_lists#index"
 end
